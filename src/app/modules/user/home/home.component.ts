94f3fcdd7db57abc3f137a68e3817742a3422a44
dungtt <trinhtiendung54@gmail.com>
2024-01-31 11:38:00 +0700

    fix scroll route change

--- src/app/modules/user/home/home.component.ts
+++ src/app/modules/user/home/home.component.ts
@@ -50,31 +50,27 @@ export class HomeComponent implements OnInit {
     this.images = [
       {
         itemImageSrc: '../../../assets/medias/carousel/carousel-1.jpg',
-        thumbnailImageSrc: '../../../assets/medias/carousel/carousel-1.jpg',
         description: 'Description for Image 1',
-        title: 'Title 1'
+        title: 'Title 1',
+        url: 'https://www.google.com'
       },
       {
         itemImageSrc: '../../../assets/medias/carousel/carousel-2.jpg',
-        thumbnailImageSrc: '../../../assets/medias/carousel/carousel-2.jpg',
         description: 'Description for Image 2',
         title: 'Title 2'
       },
       {
         itemImageSrc: '../../../assets/medias/carousel/carousel-3.jpg',
-        thumbnailImageSrc: '../../../assets/medias/carousel/carousel-3.jpg',
         description: 'Description for Image 3',
         title: 'Title 3'
       },
       {
         itemImageSrc: '../../../assets/medias/carousel/carousel-4.jpg',
-        thumbnailImageSrc: '../../../assets/medias/carousel/carousel-4.jpg',
         description: 'Description for Image 4',
         title: 'Title 4'
       },
       {
         itemImageSrc: '../../../assets/medias/carousel/carousel-5.jpg',
-        thumbnailImageSrc: '../../../assets/medias/carousel/carousel-5.jpg',
         description: 'Description for Image 5',
         title: 'Title 5'
       }
